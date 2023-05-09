91eaf4426de7327d12e20149259f64e6c448d785
AlfianHidayat03 <alfinhidayat2017@gmail.com>
2023-05-09 20:06:52 +0700

    routes

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\SubcategoryController;
 use App\Http\Controllers\SliderController;
 use App\Http\Controllers\MemberController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ReportController;
 use App\Http\Controllers\TestimoniController;
 use App\Http\Controllers\ReviewController;
 use Illuminate\Http\Request;
@@ -40,4 +41,5 @@ Route::group([
    Route::get('order/diterima',[OrderController::class, 'diterima']);
    Route::get('order/selesai',[OrderController::class, 'selesai']);
    Route::post('order/ubah_status/{order}',[OrderController::class, 'ubah_status']);
+   Route::get('reports',[ReportController::class, 'index']);
 });
