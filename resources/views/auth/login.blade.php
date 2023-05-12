90768358acf662118b44ab259c4cc5e3e7c20f81
AlfianHidayat03 <alfinhidayat2017@gmail.com>
2023-05-12 22:32:34 +0700

    Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,16 +9,16 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Login</title>
+    <title>Login</title>
 
     <!-- Custom fonts for this template-->
-    <link href="sbadmin2/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="/sbadmin2/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="/https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="sbadmin2/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="/sbadmin2/css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
@@ -37,15 +37,29 @@
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                     </div>
-                                        <form class="user">
+                                    @if ($errors->any())
+                                    <div class="alert alert-danger">
+                                        <strong>Gagal</strong>
+                                        <p>{{$errors->first()}}</p>
+                                    </div>
+                                    @endif
+                                        <form class="user" method="POST" action="/login">
+                                            @csrf
                                                 <div class="form-group">
                                                     <input type="email" class="form-control form-control-user"
                                                         id="exampleInputEmail" aria-describedby="emailHelp"
-                                                        placeholder="Enter Email Address...">
+                                                        placeholder="Enter Email Address..." name="email">
+                                                        @error('email')
+                                                        <small class="text-danger">{{$message}}</small>
+                                                        @enderror
                                                 </div>
+                                              
                                                 <div class="form-group">
                                                     <input type="password" class="form-control form-control-user"
-                                                        id="exampleInputPassword" placeholder="Password">
+                                                        id="exampleInputPassword" placeholder="Password" name="password">
+                                                        @error('password')
+                                                        <small class="text-danger">{{$message}}</small>
+                                                        @enderror
                                                 </div>
                                                 <div class="form-group">
                                                     <div class="custom-control custom-checkbox small">
@@ -54,9 +68,9 @@
                                                             Me</label>
                                                     </div>
                                                 </div>
-                                                <a href="sbadmin2/index.html" class="btn btn-primary btn-user btn-block">
-                                                    Login
-                                                </a>
+                                                <button type="submit" class="btn btn-primary btn-user btn-block">
+                                                    Login 
+                                                </button>
                                             </a>
                                          </form>
                             
