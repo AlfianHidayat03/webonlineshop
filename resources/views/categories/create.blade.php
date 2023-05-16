88d274a1aa53441993716ecc94fa8d3469968995
AlfianHidayat03 <alfinhidayat2017@gmail.com>
2023-05-16 14:52:45 +0700

    create

---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tambah Data Kategori</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body style="background: lightgray">
+
+    <div class="container mt-5 mb-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card border-0 shadow-sm rounded">
+                    <div class="card-body">
+                        <form action="{{ route('categories.store') }}" method="POST" enctype="multipart/form-data">
+                        
+                            @csrf
+                            <div class="form-group">
+                                <label class="font-weight-bold">Id Kategori</label>
+                                <input type="number" class="form-control @error('id_kategori') is-invalid @enderror" name="id_kategori" value="{{ old('id_kategori') }}" placeholder="Id Kategori">
+                            
+                                <!-- error message untuk Id Kategori -->
+                                @error('id_kategori')
+                                    <div class="alert alert-danger mt-2">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label class="font-weight-bold">Nama Kategori</label>
+                                <input type="text" class="form-control @error('nama_kategori') is-invalid @enderror" name="nama_kategori" value="{{ old('nama_kategori') }}" placeholder="Nama Kategori">
+                            
+                                <!-- error message untuk Nama Kategori -->
+                                @error('nama_kategori')
+                                    <div class="alert alert-danger mt-2">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+
+
+                            <div class="form-group">
+                                <label class="font-weight-bold">Deskripsi</label>
+                                <textarea class="form-control @error('deskripsi') is-invalid @enderror" name="deskripsi" rows="5" placeholder="deskripsi"></textarea>
+                            
+                                <!-- error message untuk Deskripsi -->
+                                @error('deskripsi')
+                                    <div class="alert alert-danger mt-2">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label class="font-weight-bold">Gamnbar</label>
+                                <input type="file" class="form-control @error('gambar') is-invalid @enderror" name="gambar">
+                            
+                                <!-- error message untuk Ganbar -->
+                                @error('gambar')
+                                    <div class="alert alert-danger mt-2">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+
+                            <button type="submit" class="btn btn-md btn-primary">SIMPAN</button>
+                            <button type="reset" class="btn btn-md btn-warning">RESET</button>
+
+                        </form> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
+<script>
+    CKEDITOR.replace( 'deskripsi' );
+</script>
+</body>
+</html>
