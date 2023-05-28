e50710b8d18896a7fb48fbda291a4f2d1759f755
AlfianHidayat03 <alfinhidayat2017@gmail.com>
2023-05-28 10:47:44 +0700

    M

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -133,7 +133,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Pendataan Gudang Toko 2023 </span>
+                        <span>Copyright &copy; Data Gudang Toko 2023 </span>
                     </div>
                 </div>
             </footer>
